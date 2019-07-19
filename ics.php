d462ee7345ad436381237227101335ed7952fbbe
1221221212 <tdj_naoki@yahoo.co.jp>
2019-07-19 19:15:41 +0900

    xxxxx

--- ics.php
+++ ics.php
@@ -3,7 +3,7 @@
 $ics_content = $_POST['ics_content'];
 if (isset($_POST['ics_content'])) {
 $ics_content = filter_input(INPUT_POST, 'ics_content');
-$filepath = "worktime.ics";
+$filepath = "https://1221221212.github.io/shift-calculator.github.io/worktime.ics";
 if (($fp = fopen($filepath, 'w')) !== false) {
 flock($fp, LOCK_EX);
 fwrite($fp,$ics_content);
